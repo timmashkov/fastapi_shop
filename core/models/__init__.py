1740365c643b734cd29dd4494e9d06537c71d3ef
timmashkov <bodomizer@yandex.ru>
2023-11-21 10:00:49 +0300

    added new model, created table with alembic and made a new migration

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,9 +2,11 @@ __all__ = (
     'Base',
     'db_helper',
     'DataBaseHelper',
-    'Product'
+    'Product',
+    'User'
 )
 
 from .base import Base
 from .product import Product
+from .user import User
 from .db_helper import db_helper, DataBaseHelper
