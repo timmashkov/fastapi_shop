a06c201705c0270fe1f5efc4fa520fb25a7c2b98
timmashkov <bodomizer#yandex.ru>
2023-11-21 10:20:16 +0300

    created new model and table via alembic

---
+++ core/models/post.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String, Text,ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models.base import Base
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    title: Mapped[str] = mapped_column(String(100), nullable=False, unique=False)
+    body: Mapped[str] = mapped_column(Text, default="", server_default="")
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
