b82c697c572574f4f67ecb282ed9fafba43cb86e
timmashkov <bodomizer@yandex.ru>
2023-11-21 10:29:48 +0300

    added relationships between user and post models

--- core/models/user.py
+++ core/models/user.py
@@ -1,9 +1,16 @@
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
-
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING
 from core.models.base import Base
 
+
+if TYPE_CHECKING:
+    from .post import Post
+
+
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = "user"
 
     username: Mapped[str] = mapped_column(String(20), unique=True)
+
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
