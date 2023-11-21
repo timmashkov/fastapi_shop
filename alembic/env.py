df93431ee4ba5cba9bc516a5c4714a3948393284
timmashkov <bodomizer#yandex.ru>
2023-11-21 09:41:31 +0300

    launched first migration, added some to readme

--- alembic/env.py
+++ alembic/env.py
@@ -11,7 +11,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', settings.db_url)
+config.set_main_option("sqlalchemy.url", settings.db_url)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
