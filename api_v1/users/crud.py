35242cb7fb8052561d1efdf39fa86e1262b9bb27
timmashkov <bodomizer#yandex.ru>
2023-11-22 10:49:49 +0300

    added profile crud and view functions. tested them

--- api_v1/users/crud.py
+++ api_v1/users/crud.py
@@ -5,10 +5,10 @@ Update
 Delete
 """
 from sqlalchemy.engine import Result
-from sqlalchemy import select
+from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
-from core.models import User
-from .schemas import UserAddingSchema
+from core.models import User, Profile
+from .schemas import UserAddingSchema, ProfileAddingSchema
 
 async def get_users(session: AsyncSession) -> list[User]:
     """
@@ -35,32 +35,71 @@ async def add_user(session: AsyncSession, user_in: UserAddingSchema) -> User:
     return users
 
 
-async def get_user(session: AsyncSession, user_id: int) -> User | None:
+async def add_user_profile(session: AsyncSession,
+                           user_id: int,
+                           data: ProfileAddingSchema) -> Profile:
+    profile = Profile(user_id=user_id,
+                      first_name=data.first_name,
+                      last_name=data.last_name,
+                      bio=data.bio)
+    session.add(profile)
+    await session.commit()
+    return profile
+
+
+async def edit_user_profile(first_name: str,
+                            session: AsyncSession,
+                            data: ProfileAddingSchema):
+    stmt = update(Profile)\
+        .where(Profile.first_name == first_name)\
+        .values(first_name=data.first_name,
+                last_name=data.last_name,
+                bio=data.bio).returning(Profile.id,
+                                        Profile.first_name,
+                                        Profile.last_name,
+                                        Profile.bio,
+                                        Profile.user_id)
+    result: Result = await session.execute(stmt)
+    answer = result.scalars().all()
+    await session.commit()
+    return answer
+
+
+async def get_user(session: AsyncSession, username: str):
     """
     Функция для выбора юзера по id
     :param session:
-    :param user_id:
+    :param username:
     :return: User
     """
-    return await session.get(User, user_id)
+    stmt = select(User).where(User.username == username)
+    result: Result = await session.execute(stmt)
+    user: User | None = result.scalar_one_or_none()
+    if user:
+        return user
+    else:
+        return {'error': f"There is no {username}"}
 
 
-async def change_user(session: AsyncSession,
-                      user: User,
-                      data: UserAddingSchema,
-                      partial: bool = False) -> User:
+async def change_user(username: str,
+                      session: AsyncSession,
+                      data: UserAddingSchema):
     """
     Функция для изменения данных юзера
+    :param username:
     :param session:
-    :param user:
     :param data:
-    :param partial:
     :return:
     """
-    for name, value in data.model_dump(exclude_unset=partial).items():
-        setattr(User, name, value)
+    stmt = update(User)\
+        .where(User.username == username)\
+        .values(username=data.username)\
+        .returning(User.id,
+                   User.username)
+    result: Result = await session.execute(stmt)
     await session.commit()
-    return user
+    answer = result.scalars().all()
+    return answer
 
 
 async def drop_user(session: AsyncSession, user: User) -> None:
@@ -72,3 +111,17 @@ async def drop_user(session: AsyncSession, user: User) -> None:
     """
     await session.delete(user)
     await session.commit()
+
+
+async def drop_user_profile(first_name: str, session: AsyncSession):
+    stmt = delete(Profile)\
+        .where(Profile.first_name == first_name)\
+        .returning(Profile.id,
+                   Profile.first_name,
+                   Profile.last_name,
+                   Profile.bio,
+                   Profile.user_id)
+    profile: Result = await session.execute(stmt)
+    answer = profile.scalars().all()
+    await session.commit()
+    return answer
--- api_v1/users/views.py
+++ api_v1/users/views.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from .crud import get_users, add_user, get_user, change_user, drop_user
-from .schemas import UserResponseSchema, UserAddingSchema, UserUpdatePartial
+from .crud import get_users, add_user, get_user, change_user, drop_user, add_user_profile, \
+    edit_user_profile, drop_user_profile
+from .schemas import UserResponseSchema, UserAddingSchema, UserUpdatePartial, \
+    ProfileResponseSchema, ProfileAddingSchema
 from core.models import User
 from core.models import db_helper
 from .dependencies import user_by_id
@@ -15,10 +17,10 @@ async def show_users(session: AsyncSession = Depends(db_helper.scoped_session_de
     return await get_users(session=session)
 
 
-@router.get('/{user_id}', response_model=UserResponseSchema)
-async def show_user(user_id: int,
+@router.get('/{username}')
+async def show_user(username: str,
                     session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
-    return await get_user(session=session, user_id=user_id)
+    return await get_user(session=session, username=username)
 
 
 @router.post('/create', response_model=UserResponseSchema)
@@ -27,17 +29,36 @@ async def create_user(data: UserAddingSchema,
     return await add_user(session=session, user_in=data)
 
 
-@router.put('/{upd_id}')
-async def update_user(user_in: UserUpdatePartial,
-                      user: User = Depends(user_by_id),
+@router.post('/create_profile/{user_id}', response_model=ProfileResponseSchema)
+async def create_profile(user_id: int,
+                         data: ProfileAddingSchema,
+                         session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
+    return await add_user_profile(session=session, user_id=user_id, data=data)
+
+
+@router.put('/{username}')
+async def update_user(username: str,
+                      user_in: UserUpdatePartial,
                       session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
     return await change_user(session=session,
-                             user=user,
-                             data=user_in,
-                             partial=True)
+                             username=username,
+                             data=user_in)
+
+
+@router.put('/profile/{first_name}')
+async def update_profile(first_name: str,
+                         profile_in: ProfileAddingSchema,
+                         session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
+    return await edit_user_profile(first_name=first_name, data=profile_in, session=session)
 
 
 @router.delete('/{id}')
 async def delete_user(user: User = Depends(user_by_id),
                       session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
     return await drop_user(session=session, user=user)
+
+
+@router.delete('/profile/{first_name}')
+async def delete_profile(first_name: str,
+                         session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
+    return await drop_user_profile(first_name=first_name, session=session)
