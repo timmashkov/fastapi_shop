8c372a6b7bc1faa5f187b9167e2da56e977762be
timmashkov <bodomizer#yandex.ru>
2023-12-01 10:45:25 +0300

    some minor changes

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from .products.views import router as products_router
 from .users.views import router as users_router
+from .profiles.views import router as profile_router
 
 router = APIRouter(
     prefix='/api_v1'
@@ -8,3 +9,4 @@ router = APIRouter(
 
 router.include_router(router=products_router)
 router.include_router(router=users_router)
+router.include_router(router=profile_router)
--- api_v1/users/views.py
+++ api_v1/users/views.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from .crud import get_users, add_user, get_user, change_user, drop_user, add_user_profile, \
-    edit_user_profile, drop_user_profile, get_user_with_profile
-from .schemas import UserResponseSchema, UserAddingSchema, UserUpdatePartial, \
-    ProfileResponseSchema, ProfileAddingSchema, UserWithProfile
+from .crud import get_users, add_user, get_user, change_user, drop_user, get_user_with_profile
+from .schemas import UserResponseSchema, UserAddingSchema, UserUpdatePartial, UserWithProfile
 from core.models import User
 from core.models import db_helper
 from .dependencies import user_by_id
@@ -11,6 +9,7 @@ from .dependencies import user_by_id
 router = APIRouter(
     prefix='/users'
 )
+
 
 @router.get('/', response_model=list[UserResponseSchema])
 async def show_users(session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
@@ -35,14 +34,7 @@ async def create_user(data: UserAddingSchema,
     return await add_user(session=session, user_in=data)
 
 
-@router.post('/create_profile/{user_id}', response_model=ProfileResponseSchema)
-async def create_profile(user_id: int,
-                         data: ProfileAddingSchema,
-                         session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
-    return await add_user_profile(session=session, user_id=user_id, data=data)
-
-
-@router.put('/{username}')
+@router.put('/{username}', response_model=UserUpdatePartial)
 async def update_user(username: str,
                       user_in: UserUpdatePartial,
                       session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
@@ -51,20 +43,7 @@ async def update_user(username: str,
                              data=user_in)
 
 
-@router.put('/profile/{first_name}')
-async def update_profile(first_name: str,
-                         profile_in: ProfileAddingSchema,
-                         session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
-    return await edit_user_profile(first_name=first_name, data=profile_in, session=session)
-
-
 @router.delete('/{id}')
 async def delete_user(user: User = Depends(user_by_id),
                       session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
     return await drop_user(session=session, user=user)
-
-
-@router.delete('/profile/{first_name}')
-async def delete_profile(first_name: str,
-                         session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
-    return await drop_user_profile(first_name=first_name, session=session)
