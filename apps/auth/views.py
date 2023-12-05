b7b6db96ad20316ab33ed5b3de3fe16942dc34ba
timmashkov <bodomizer#yandex.ru>
2023-12-05 14:47:59 +0300

    some minor changes

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,7 +1,7 @@
 import secrets
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Header
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 from starlette import status
 
@@ -12,26 +12,3 @@ router = APIRouter(
 security = HTTPBasic()
 
 
-@router.get("/auth")
-async def get_auth(credential: Annotated[HTTPBasicCredentials, Depends(security)]):
-    return {'message': 'hello', 'data': credential}
-
-
-data = {'admin': 'admin', 'user': 'password'}
-
-
-async def take_username(credential: Annotated[HTTPBasicCredentials, Depends(security)]):
-    unauth = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid data")
-    correct_password = data.get(credential.username)
-    if correct_password is None:
-        raise unauth
-    if credential.username not in data:
-        raise unauth
-    if not secrets.compare_digest(credential.password.encode("utf-8"), correct_password.encode("utf-8")):
-        raise unauth
-    return credential.username
-
-
-@router.get("/auth")
-async def get_auth_user(auth_username: str = Depends(take_username)):
-    return {'message': 'hello', 'data': auth_username}
