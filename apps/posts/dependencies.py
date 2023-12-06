d6043ee2b8617e8d0ebec16bd805e93cbc24100c
timmashkov <bodomizer@yandex.ru>
2023-12-06 17:32:31 +0300

    added dependecy to apps/post.dependencies/ to find post by id

---
+++ apps/posts/dependencies.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from fastapi import Path, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from apps.posts.crud import get_post
+from core.database import vortex
+
+
+async def post_by_id(
+    post_in: Annotated[int, Path],
+    session: AsyncSession = Depends(vortex.scoped_session_dependency),
+):
+    post = await get_post(session=session, post_id=post_in)
+    if post:
+        return post
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Post {post_in} not found!",
+    )
