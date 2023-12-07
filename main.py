98de0e13e3a50566a65ff309bf3a29393830f594
timmashkov <bodomizer@yandex.ru>
2023-12-07 09:32:48 +0300

    added new things to settings, added templates with static, added tasks for celery tasks

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
+from starlette.responses import HTMLResponse
+
 from apps import router as apps_router
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI(title="Learning FastAPI")
 #TODO: удалить apps/users
 app.include_router(apps_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items")
+async def read_item(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request})
 
 
 @app.on_event("startup")
