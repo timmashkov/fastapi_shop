9fb9065a7f8cbbd0124009233207642d02ca189d
timmashkov <bodomizer@yandex.ru>
2023-12-08 14:02:01 +0300

    enabled celery, flower and sending email as background process

--- apps/tasks/mail.py
+++ apps/tasks/mail.py
@@ -1,13 +1,35 @@
-from email.mime.text import MIMEText
-from smtplib import SMTP_SSL
+import smtplib
+from email.message import EmailMessage
 
 from celery import Celery
-from fastapi import BackgroundTasks
-from pydantic import BaseConfig
 
 from core.config import settings
-from fastapi_mail import ConnectionConfig, MessageSchema, FastMail
+
 
 celery = Celery("tasks", broker="redis://localhost:6379")
 
 
+def get_email_template_dashboard(username: str):
+    email = EmailMessage()
+    email['Subject'] = 'Натрейдил Отчет Дашборд'
+    email['From'] = settings.MAIL_FROM
+    email['To'] = settings.MAIL_USERNAME
+
+    email.set_content(
+        '<div>'
+        f'<h1 style="color: red;">Здравствуйте, {username}, а вот и ваш отчет. Зацените 😊</h1>'
+        '<img src="https://static.vecteezy.com/system/resources/previews/008/295/031/original/custom-relationship'
+        '-management-dashboard-ui-design-template-suitable-designing-application-for-android-and-ios-clean-style-app'
+        '-mobile-free-vector.jpg" width="600">'
+        '</div>',
+        subtype='html'
+    )
+    return email
+
+
+@celery.task
+def send_email_report_dashboard(username: str):
+    email = get_email_template_dashboard(username)
+    with smtplib.SMTP_SSL(settings.MAIL_SERVER, settings.MAIL_PORT) as server:
+        server.login(settings.MAIL_USERNAME, settings.GOOGLE_API_PASS)
+        server.send_message(email)
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,8 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks, Depends
+
+from apps.auth import current_user
+from apps.tasks.mail import send_email_report_dashboard
+
+router = APIRouter(prefix="/tasks")
 
 
-router = APIRouter(
-    prefix="/tasks"
-)
-
-
+@router.get("/dashboard")
+def get_dashboard_report(background_tasks: BackgroundTasks, user=Depends(current_user)):
+    # 1400 ms - Клиент ждет
+    send_email_report_dashboard(user.username)
+    # 500 ms - Задача выполняется на фоне FastAPI в event loop'е или в другом треде
+    background_tasks.add_task(send_email_report_dashboard, user.username)
+    # 600 ms - Задача выполняется воркером Celery в отдельном процессе
+    send_email_report_dashboard.delay(user.username)
+    return {
+        "status": 200,
+        "data": "Письмо отправлено",
+        "details": None
+    }
