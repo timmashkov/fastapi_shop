893b26026e70fb1514537302d696406d23fd2ccc
timmashkov <bodomizer#yandex.ru>
2023-12-28 23:23:29 +0300

    some minor changes

--- docker/celery.sh
+++ docker/celery.sh
@@ -1,8 +1,4 @@
 #! /bin/sh
-
-if [[ "${1}" == "celery" ]]; then
-  celery --app=apps.tasks.mail:celery worker -l INFO
-elif [[ "${1}" == "flower" ]]; then
-  celery --app=apps.tasks.mail:celery flower
- fi
- exec "$@"
+celery --app=apps.tasks.mail:celery worker -l INFO
+celery --app=apps.tasks.mail:celery flower
+exec "$@"
