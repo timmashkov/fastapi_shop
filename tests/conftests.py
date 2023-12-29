910c5fbd8757d6a02b363efa9a6e3d97e8545df7
timmashkov <bodomizer@yandex.ru>
2023-12-30 00:17:31 +0300

    some minor changes

--- tests/conftests.py
+++ tests/conftests.py
@@ -1,7 +1,7 @@
 import asyncio
 from typing import AsyncGenerator
 
-import pytest_asyncio
+import pytest
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -12,7 +12,7 @@ from core.test_database import test_database
 from main import app
 
 
-Base.metadata.bind = test_database.test_engine
+#Base.metadata.bind = test_database.test_engine
 
 
 async def override_session_dependency() -> AsyncGenerator[AsyncSession, None]:
@@ -20,10 +20,10 @@ async def override_session_dependency() -> AsyncGenerator[AsyncSession, None]:
         yield session
 
 
-app.dependency_overrides[vortex.session_dependency] = override_session_dependency
+app.dependency_overrides[vortex.scoped_session_dependency] = override_session_dependency
 
 
-@pytest_asyncio.fixture(scope="session")
+@pytest.fixture
 def event_loop(request):
     """Create an instance of the default event loop for each test case."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
@@ -31,10 +31,24 @@ def event_loop(request):
     loop.close()
 
 
+@pytest.fixture(scope="session")
+def create_db():
+    from alembic import context, command
+    from core.config import settings
+
+    config = context.config
+    config.set_main_option("sqlalchemy.url", settings.test_config.test_url)
+
+    command.upgrade(config, "head")
+    yield
+    print("Test revisions done!")
+    command.downgrade(config, "base")
+
+
 client = TestClient(app)
 
 
-@pytest_asyncio.fixture(scope="session")
+@pytest.fixture
 async def ac() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
